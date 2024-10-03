b3a58ee96ec360e69497caf19847edb115289e6f
= <=>
2024-10-03 18:51:22 -0400

    Create Manager class with inheritance from Employee

--- employee_management.js
+++ employee_management.js
@@ -6,7 +6,7 @@ constructor(name, salary, position, department ) {
     this._department = department;
 }
 getDetails() {
-    console.log(`Name: ${this._name}, Salary: ${this._salary}, Position: ${this._position}`)
+    return (`Name: ${this._name}, Salary: ${this._salary}, Position: ${this._position}`)
 }
 
 }
@@ -32,6 +32,22 @@ get departmentSalary () {
 const tech = new Department ('tech', [])
 tech.addEmployee(Maria)
 tech.addEmployee(Sara)
-console.log(tech)
+// console.log(tech)
 
-console.log(tech.departmentSalary)
+// console.log(tech.departmentSalary)
+
+
+
+class Manager extends Employee {
+    constructor(name, salary, position, department, bonus) {
+        super(name, salary, position, department );
+        this._bonus = bonus;
+    }
+
+    getDetails () {
+        return (`${super.getDetails()}, Bonus: ${this._bonus}`)
+    }
+}
+
+const Manuela = new Manager ('Manuela', 68000, 'analyst', 'tech', 2000)
+console.log(Manuela.getDetails())
